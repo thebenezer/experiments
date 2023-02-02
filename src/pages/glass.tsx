bd302d295943ce41f62879b4b5a278c2fa79e50e
Ebenezer <58320436+thebenezer@users.noreply.github.com>
2023-02-02 19:10:06 +0530

    added seo tags

--- src/pages/glass.tsx
+++ src/pages/glass.tsx
@@ -13,6 +13,22 @@ const Glass: NextPage = () => {
         <title>Glass</title>
         <meta name="description" content="Glass" />
         <link rel="icon" href="/favicon.ico" />
+        
+        <meta itemProp="name" content="Ebenezer - Experiment"></meta>
+        <meta itemProp="description" content="River and Waterfall Shader built with three.js and theatre.js."></meta>
+        <meta itemProp="image" content="/waterfallAssets/Social.webp"></meta>
+
+        <meta name="twitter:card" content="summary_large_image"></meta>
+        <meta name="twitter:title" content="Ebenezer - Experiment"></meta>
+        <meta name="twitter:description" content="River and Waterfall Shader built with three.js and theatre.js."></meta>
+        <meta name="twitter:image" content="/waterfallAssets/Social.webp"></meta>
+
+        <meta property="og:site_name" content="Ebenezer - Experiment"></meta>
+        <meta property="og:type" content="website"></meta>
+        <meta property="og:url" content="https://thebenezer.com"></meta>
+        <meta property="og:title" content="Ebenezer - Experiment"></meta>
+        <meta property="og:description" content="River and Waterfall Shader built with three.js and theatre.js."></meta>
+        <meta property="og:image" content="/waterfallAssets/Social.webp"></meta>
       </Head>
       <GlassUI></GlassUI>
       <GlassCanvas></GlassCanvas>
